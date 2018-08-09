d73e324f22712d57d1e9a8d5d982831187053309
karthik4h1 <42196570+karthik4h1@users.noreply.github.com>
2018-08-09 11:25:05 +0530

    Update positive.py

--- positive.py
+++ positive.py
@@ -1,7 +1,8 @@
-a=int(raw_input())
-  if(a>0):
-      print("positive")
-   elif(a<0):
-      print("negative")
-    else:
-      print("zero")
+a=int(input())
+if(a>0):
+      print('Positive')
+elif(a<0):
+      print('Negative')
+else:
+      print('Zero')
+
