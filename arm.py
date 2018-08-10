7931fb21b2f6139acd45fc848f837a3af5d2e679
karthik4h1 <42196570+karthik4h1@users.noreply.github.com>
2018-08-10 10:53:31 +0530

    Create arm.py

---
+++ arm.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+p=len(str(num))
+sum = 0
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** p
+   temp //= 10
+if num == sum:
+   print(num,"yes")
+else:
+   print(num,"no")
