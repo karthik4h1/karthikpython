090b5d2d919b1d687d0c49b547a68be91751f99b
karthik4h1 <42196570+karthik4h1@users.noreply.github.com>
2018-08-11 16:48:16 +0530

    Update absvalue.py

--- absvalue.py
+++ absvalue.py
@@ -1,7 +1,7 @@
-a0,b0=map(int,input().split())
-a1,b1=map(int,input().split())
-c0=(a0*60)+b0
-d0=(a1*60)+b1
+e0,b0=map(int,input().split())
+e1,b1=map(int,input().split())
+c0=(e0*60)+b0
+d0=(e1*60)+b1
 d1=c0-d0
 c1=d1//60
 d1%=60
