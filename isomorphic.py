92ed5a46a422ca75bedba294bd51b97e00ddf4d3
karthik4h1 <42196570+karthik4h1@users.noreply.github.com>
2018-08-13 14:40:09 +0530

    Update isomorphic.py

--- isomorphic.py
+++ isomorphic.py
@@ -5,16 +5,16 @@ def iso(string1, string2):
 	m = len(string1)
 	n = len(string2)
 	if m != n:
-		return "Not isomorphic."
+		return "No"
 	t = [False] * MAX_CHARS
 	map = [-1] * MAX_CHARS
 	for i in range(n):
 		if map[ord(string1[i])] == -1:
 			if t[ord(string2[i])] == True:
-			        return "Not isomorphic."
+			        return "No"
 			t[ord(string2[i])] = True
 			map[ord(string1[i])] = string2[i]
 		elif map[ord(string1[i])] != string2[i]:
-			return "Not isomorphic."
+			return "No"
 	return "Yes" 
 print (iso(a1,a2))
