fa6ada2b69695b85fe60b6bde72e605551ea836a
karthik4h1 <42196570+karthik4h1@users.noreply.github.com>
2018-08-16 14:39:52 +0530

    Create liesameplane.py

---
+++ liesameplane.py
@@ -0,0 +1,9 @@
+x1,y1=map(int,input().split())
+x2,y2=map(int,input().split())
+x3,y3=map(int,input().split())
+
+if ((x1*(y1-y2)+x2*(y2-y3)+x3*(y3-y1))==0):
+    print ("yes")
+else:
+     print ("no")
+
