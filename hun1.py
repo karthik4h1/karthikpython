7d4ed41f1a25da2cb604199e088615f7c224e813
karthik4h1 <42196570+karthik4h1@users.noreply.github.com>
2018-08-17 15:54:39 +0530

    Create hun1.py

---
+++ hun1.py
@@ -0,0 +1,16 @@
+k=raw_input()
+q=0
+w=0
+new=[]
+list=[int(x) for x in raw_input().split()]
+while(q==0):
+    l=list[q]
+    list.remove(list[0])
+    if((l in list)and(l not in new)):
+        print l,
+        new.insert(w,l)
+        w+=1
+    elif((new==[])and(list==[])):
+        print('unique')
+    if(list==[]):
+        break
